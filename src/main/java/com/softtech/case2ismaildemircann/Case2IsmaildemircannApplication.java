0bc348185757bef6aea32da6a842be3dee610281
ismaildemircan <ismaildemircann98@gmail.com>
2022-02-17 15:41:33 +0300

    Create empty spring boot project

---
+++ src/main/java/com/softtech/case2ismaildemircann/Case2IsmaildemircannApplication.java
@@ -0,0 +1,13 @@
+package com.softtech.case2ismaildemircann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case2IsmaildemircannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Case2IsmaildemircannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softtech/case2ismaildemircann/Case2IsmaildemircannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtech.case2ismaildemircann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case2IsmaildemircannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
